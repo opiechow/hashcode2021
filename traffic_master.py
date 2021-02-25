7f1bda5b66f56ac6a2d600066eecc4db9452262d
Oskar Piechowski <ospiechowski@gmail.com>
2021-02-25 19:42:52 +0100

    next

--- traffic_master.py
+++ traffic_master.py
@@ -1,6 +1,18 @@
 in_file = "inputs/a.txt"
 
+streets = {}
+car_routes = []
+
 with open(in_file, "r") as f:
     first_line = f.readline()
-    print(first_line.split())
+    duration, num_intersections, num_streets, num_cars, bonus_points = map(lambda x: int(x), first_line.split())
+    for i in range(num_streets):
+        pass
+    for i in range(num_cars):
+        pass
+
+print("duration: {}. num_intersections: {}, num_streets: {}, num_cars: {}, bonus_points: {} "
+      .format(duration, num_intersections, num_streets, num_cars, bonus_points))
+
+
 
