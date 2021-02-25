dde9361b1ad31be4312e174f168e00cf16cdb410
Oskar Piechowski <ospiechowski@gmail.com>
2021-02-25 19:55:24 +0100

    reading data

--- traffic_master.py
+++ traffic_master.py
@@ -7,12 +7,17 @@ with open(in_file, "r") as f:
     first_line = f.readline()
     duration, num_intersections, num_streets, num_cars, bonus_points = map(lambda x: int(x), first_line.split())
     for i in range(num_streets):
-        pass
+        street_line = f.readline()
+        elems = street_line.split()
+        streets[elems[2]] = {"beg" : int(elems[0]), "end" : int(elems[1]), "cost" : int(elems[3])}
     for i in range(num_cars):
-        pass
+        car_line = f.readline()
+        car_routes.append(car_line.split()[1:])
 
 print("duration: {}. num_intersections: {}, num_streets: {}, num_cars: {}, bonus_points: {} "
       .format(duration, num_intersections, num_streets, num_cars, bonus_points))
+print("streets: {}".format(streets))
+print("car routes: {}".format(car_routes))
 
 
 
